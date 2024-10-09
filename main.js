a902eabd59faef0e0f1ba1c13722f1e68c4fe85f
Jared Mc Allister <jaredmcallister@Mac.attlocal.net>
2024-10-09 12:47:38 -0500

    first attempt

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstNum = 10;
+const secondNum = firstNum * 4;
+const thirdNum = secondNum -1;
+const combo = `${firstNum} ${secondNum} ${thirdNum}`
+console.log(`This is the combo: ${combo}`)
+console.log(firstNum)
+console.log(secondNum)
+window.alert (`You have received this message because you have been chosen to open an important vault. Here is the secret combination ${combo}`); 
